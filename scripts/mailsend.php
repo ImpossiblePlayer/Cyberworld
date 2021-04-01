2d716e027cf94aff800d75093f4babe8da18eea1
ImpossiblePlayer <70282102+ImpossiblePlayer@users.noreply.github.com>
2021-04-01 23:12:51 +0500

    Add files via upload

---
+++ scripts/mailsend.php
@@ -0,0 +1,15 @@
+<?php 
+	if(isset($_POST['submit'])){
+	    $to = "ivanpronin1379@gmail.com"; // This is Email address where email comes / Это адрес получателя
+	    $from = $_POST['email']; // This is the sender's Email address / Это адрес отправителя
+	    $name = $_POST['name'];
+	    $subject = "Form submission";
+	    $subject2 = "Copy of your form submission";
+	    $message = $name . " отправил:" . "\n\n" . $_POST['message'];
+	    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+	    $headers = "From: " . $from . ",\n" . $tel;
+	    mail($to,$subject,$message,$headers); // Sends message on Email / Отправляет письмо на Email
+	    echo "Письмо отправлено. Спасибо,  " . $name . ", Вам скоро ответят.";
+	    }
+?>
