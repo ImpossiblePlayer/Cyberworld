0a21946524a92c1ea41f678c21b2bb15b1ab87fb
ImpossiblePlayer <70282102+ImpossiblePlayer@users.noreply.github.com>
2021-04-02 17:32:02 +0500

    Add files via upload

--- scripts/mailsend.php
+++ scripts/mailsend.php
@@ -1,15 +1,13 @@
 <?php 
-if(isset($_POST['submit'])){
-    $to = "ivanpronin@gmail.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-	$tel = $_POST['tel'];
-    $name = $_POST['name'];
-    $subject = "Form submission";
-    $message = $name . " (" . $tel . ") " . " написал:" . "\n\n" . $_POST['message'];
+	if(isset($_POST['submit'])){
+	    $to = "ivanpronin@gmail.com"; // this is your Email address
+	    $from = $_POST['email']; // this is the sender's Email address
+		$tel = $_POST['tel'];
+	    $name = $_POST['name'];
+	    $subject = "Form submission";
+	    $message = $name . " (" . $tel . ") " . " написал:" . "\n\n" . $_POST['message'];
 
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+	    $headers = "From:" . $from;
+	    mail($to,$subject,$message,$headers);
     }
 ?>
