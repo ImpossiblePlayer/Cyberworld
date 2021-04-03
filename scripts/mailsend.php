c76c13a6481124a8a6e504a90f4daeda9f284ea4
ImpossiblePlayer <70282102+ImpossiblePlayer@users.noreply.github.com>
2021-04-03 15:41:27 +0500

    Add files via upload

--- scripts/mailsend.php
+++ scripts/mailsend.php
@@ -1,13 +1,13 @@
 <?php 
-	if(isset($_POST['submit'])){
-	    $to = "ivanpronin@gmail.com"; // this is your Email address
-	    $from = $_POST['email']; // this is the sender's Email address
-		$tel = $_POST['tel'];
-	    $name = $_POST['name'];
-	    $subject = "Form submission";
-	    $message = $name . " (" . $tel . ") " . " написал:" . "\n\n" . $_POST['message'];
-
-	    $headers = "From:" . $from;
-	    mail($to,$subject,$message,$headers);
+	if(isset($_POST['submit'])) { // Checks if button was used / Проверяет, была ли нажата кнопка
+	    $to = "ivanpronin@gmail.com"; // Adress where emails are sent / Адрес, куда отправляются письма
+	    $from = $_POST['email']; // User's adress / Адрес отправителя
+		$tel = $_POST['tel']; // User's phone number / Номер телефона отправителя
+	    $name = $_POST['name']; // User name / Имя отправителя
+	    $subject = "Отзыв от:" . $name;
+	    $message = $_POST['message']; // Connects user name & message
+									  // Соединяет имя отправителя и сообщение
+	    $footer = "Контакты:" . $from . ", " . $tel; // Contacts in footer / Контакты в футере
+	    mail($to,$subject,$message,$footer); // Sends all info on email / Отправляет всю информацию на почту
     }
 ?>
